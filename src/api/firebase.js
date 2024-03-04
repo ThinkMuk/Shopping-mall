5423a55ec3663ad69517a4cd66a4d05a4d0bc19d
ThinkMuk <b2000kang@gmail.com>
2024-03-04 22:58:36 +0900

    feat: login, logout features implemented  using firebase google auth

---
+++ src/api/firebase.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const provider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export async function login() {
+  provider.setCustomParameters({ prompt: "select_account" });
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+      return user;
+    })
+    .catch(console.error);
+}
+
+export async function logout() {
+  provider.setCustomParameters({ prompt: "select_account" });
+  return signOut(auth).then(() => null);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+}
