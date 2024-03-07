0a7ce37ff5c31eb32dcfaa11ef9215ecd1813e6c
ThinkMuk <b2000kang@gmail.com>
2024-03-07 23:09:00 +0900

    style: update User css

--- src/componenets/User.jsx
+++ src/componenets/User.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function User({ user: { photoURL, displayName } }) {
   return (
-    <div>
-      <img src={photoURL} alt={displayName} />
-      <span>{displayName}</span>
+    <div className="flex items-center">
+      <img
+        className="w-8 h-8 rounded-full mr-2"
+        src={photoURL}
+        alt={displayName}
+      />
+      <span className="hidden md:block">{displayName}</span>
     </div>
   );
 }
