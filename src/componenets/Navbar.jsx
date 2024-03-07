b152ed28f5e3e6ac059160000910a8a836875d4d
ThinkMuk <b2000kang@gmail.com>
2024-03-07 23:05:48 +0900

    feat: login user info display implemented

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import { TbHanger } from "react-icons/tb";
 import { BsFillPencilFill } from "react-icons/bs";
 import { login, logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
 
 export default function Navbar() {
   const [user, setUser] = useState();
   useEffect(() => {
     onUserStateChange((user) => {
-      console.log(user);
       setUser(user);
     });
   });
@@ -30,6 +30,7 @@ export default function Navbar() {
         <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
+        {user && <User user={user} />}
         {!user && <button onClick={handleLogin}>Login</button>}
         {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
